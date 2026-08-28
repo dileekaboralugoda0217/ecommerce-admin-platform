65de9c013a101c91d4088c749a70b4c6e5e71a4a
dileekaboralugoda0217 <dileekasjboralugoda@gmail.com>
2026-08-28 23:44:41 +0530

    feat: configure Sequelize PostgreSQL connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import sequelize from "./config/database.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+
+    console.log("Database connection successful!");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
